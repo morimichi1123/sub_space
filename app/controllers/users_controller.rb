4fc14638174cf75835291bbf277eee8e63915296
morimichi1123 <morisaki.michiya@lmi.ne.jp>
2019-11-11 11:38:33 +0000

    signup_done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
@@ -9,10 +10,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Space App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'users/show'
-  get 'users/create'
-
-  get 'sessions/new'
-
   root 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
   resources :users
 end
