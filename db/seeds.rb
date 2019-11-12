3bc8a7f69a827741c5a4b0d85c2ca724b86b0696
morimichi1123 <morisaki.michiya@lmi.ne.jp>
2019-11-12 12:05:23 +0000

    seedできないけど12日ラスト！

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.create!(
            password_confirmation: "morimori",
            admin: true)
 
-99999.times do |n|
+9999.times do |n|
 user_name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
@@ -18,15 +18,16 @@ end
 
 
 Space.create!(
-           space_name: "ginza",
-           ward_id: "1",
-           price: "5000",
-           )
+              space_name: "ginza",
+              ward_id: "1",
+              price: "5000",
+             )
 
 9999.times do |n|
 gimei = Gimei.new
-Space.create!(space_name: gimei.address.prefecture.kanji,
-             ward_id: rand(1..23),
-             price: rand(5000..10000)
+Space.create!(
+              space_name: gimei.address.prefecture.kanji,
+              ward_id: rand(1..23),
+              price: rand(5000..10000)
              )
 end
