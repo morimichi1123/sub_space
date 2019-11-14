c9a2cd69a889ddbfaeea7c284e5b333390081aa1
morimichi1123 <morisaki.michiya@lmi.ne.jp>
2019-11-14 04:33:13 +0000

    login_all_done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:user][:email].downcase)
     if user && user.authenticate(params[:user][:password])
       log_in user
-      redirect_to user
+      if current_user.admin?
+        redirect_to root_path
+      else
+        redirect_to list_path
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
